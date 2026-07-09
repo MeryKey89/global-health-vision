c0c201190448b74c10a46a1a50153ee948afa041
MeryKey89 <merykey89@gmail.com>
2026-07-09 14:21:13 +0200

    Actualizar app.py

--- app.py
+++ app.py
@@ -1,28 +1,21 @@
-import subprocess
-import sys
-
-# Instalación automática de librerías
-subprocess.check_call([sys.executable, "-m", "pip", "install", "google-generativeai", "pillow"])
-
 import streamlit as st
 import google.generativeai as genai
 from PIL import Image
 
-st.title("🏥 Diagnóstico IA: Análisis Clínico")
+st.title("🏥 Diagnóstico IA")
 
-# Configuración de la IA
-try:
-    genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
+# Intentamos cargar la API
+api_key = st.secrets.get("GOOGLE_API_KEY")
+if not api_key:
+    st.error("No se encuentra la API KEY. Por favor, revisa tus Secrets en Streamlit.")
+else:
+    genai.configure(api_key=api_key)
     modelo = genai.GenerativeModel('gemini-1.5-flash')
-except:
-    st.error("Configura tu GOOGLE_API_KEY en los Secrets.")
-    st.stop()
 
-archivo = st.file_uploader("Sube radiografía", type=["jpg", "png"])
-if archivo:
-    img = Image.open(archivo)
-    st.image(img)
-    if st.button('Analizar con IA'):
-        with st.spinner('Analizando...'):
+    archivo = st.file_uploader("Sube radiografía", type=["jpg", "png"])
+    if archivo:
+        img = Image.open(archivo)
+        st.image(img)
+        if st.button('Analizar'):
             respuesta = modelo.generate_content(["Actúa como radiólogo. Analiza esta imagen:", img])
             st.info(respuesta.text)
